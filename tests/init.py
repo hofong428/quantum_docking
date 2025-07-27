b38ff71f0e1aebb482b088f384361a63aab10f1e
Raymond Chang <hofong.chang@gmail.com>
2025-07-27 22:02:26 +0800

    Create init.py

---
+++ tests/init.py
@@ -0,0 +1,15 @@
+"""
+Test package for quantum docking
+"""
+
+# Test configuration
+TEST_CONFIG = {
+    'test_molecules': [
+        'CCO',  # Ethanol
+        'CCN',  # Ethylamine  
+        'c1ccccc1',  # Benzene
+        'CC(=O)O',  # Acetic acid
+    ],
+    'num_qubits': 4,
+    'max_iterations': 10
+}
